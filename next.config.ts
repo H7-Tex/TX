b7d62483c93d22d1026a13fa305f8f964585b862
AbuCTF <aburahman918@gmail.com>
2025-12-24 23:37:06 +0530

    Update v3.3

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
-  // Optional: Change the base path if your site is in a subdirectory
-  // basePath: '/repository-name',
-  // Optional: Add trailing slash
   trailingSlash: true,
+  distDir: 'out',
 };
 
 export default nextConfig;
